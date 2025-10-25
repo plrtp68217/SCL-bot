2490699eec9d1fe27072b6116635d29737687cd5
plrtp <plrtp@mail.ru>
2025-10-25 19:09:12 +0300

    update: добавлены стикеры SCL, отредактирован dockerfile

--- bot.js
+++ bot.js
@@ -7,6 +7,13 @@ const token = process.env.API_TOKEN;
 
 const bot = new TelegramBot(token, {polling: true});
 
+const stickers = [
+    'CAACAgIAAxkBAAEPo2Vo_PO_Gphb7hIJTXy4BxFu0MOpeQACzIQAAraN6EtR-qduDWN52TYE',
+    'CAACAgIAAxkBAAEPo2do_PPEGdpL0bi-xO3m8K_qPBTTXwACr4YAAo0b6UsqrZxZs-v9STYE',
+    'CAACAgIAAxkBAAEPo2lo_PPIJS-jZyPVh29qm9fQdCmVZgACGY0AAgzR6EvO92q8gxAvwzYE',
+    'CAACAgIAAxkBAAEPo2to_PPJ4ijUWkXrsi5-V193b_kQUQAC2n0AAqKn6UvH62XfGjVH7zYE',
+]
+
 const start = () => {
     bot.setMyCommands([
         {command: '/start', description: 'Старт'},
@@ -19,8 +26,8 @@ const start = () => {
         if (text == '/start') {
             const username = message.chat.username;
 
-            await bot.sendMessage(chatId, `SCL Games Beta? Hello, ${username}`);
-            return bot.sendSticker(chatId, 'CAACAgIAAxkBAAEOJFFn4AuWLyWSs-gTO8KM1W2TdLHTcQACIgADTlzSKWF0vv5zFvwUNgQ');
+            await bot.sendMessage(chatId, `SCL Games. Hello, ${username}.`);
+            await bot.sendSticker(chatId, stickers[Math.floor(Math.random() * stickers.length)]);
         }
     })
 }
